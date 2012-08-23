4abad1e7d4bb22e48764574d9af716f255f07044
Wil Black <wilblack21@gmail.com>
2012-08-23 13:35:54 -0700

    Adding contorls and features

---
+++ features.js
@@ -0,0 +1,32 @@
+/* Feature are map elements representing geographic locations. 
+ * There are 3 base features:
+ *   1. point
+ *   2. polyline
+ *   3. polygon
+ *
+ */
+ 
+ var Feature = function(type){
+    this.type=type;   
+ }
+ 
+Feature.prototype.create = function(data){
+};
+
+Feature.prototype.destroy = function(){
+};
+
+Feature.prototype.show = function(){
+};
+
+Feature.prototype.hide = function(){
+};
+
+Feature.prototype.save = function(){
+};
+
+Feature.prototype.load = function(){
+};
+
+Feature.protoype.onClick = function(){
+};
